f7710d773798bc6941156807ddbc6e6903091fa8
belle 3 <annabelle.mouat@gmail.com>
2025-10-18 17:37:28 +1000

    Staged correct file. Removed ResNet-18 for pretrained ResNet-50 as per last commit

--- recognition/siamese_network_47452109/modules.py
+++ recognition/siamese_network_47452109/modules.py
@@ -6,91 +6,33 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torch
-
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, in_channels, out_channels, stride=1):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        #self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_channels != self.expansion*out_channels:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, self.expansion*out_channels, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*out_channels)
-            )
-
-    def forward(self, x: torch.Tensor):
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = F.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-class ResNet18(nn.Module):
-    def __init__(self,block, num_classes=10):
-        super(ResNet18, self).__init__()
-        self.in_channels = 64
-
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-
-        #self.relu = nn.ReLU(inplace=True)
-        #self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-
-        self.layer1 = self._make_layer(BasicBlock, 64, 2, stride=1)
-        self.layer2 = self._make_layer(BasicBlock, 128, 2, stride=2)
-        self.layer3 = self._make_layer(BasicBlock, 256, 2, stride=2)
-        self.layer4 = self._make_layer(BasicBlock, 512, 2, stride=2)
-
-        #self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512*block.expansion, num_classes)
-
-    def _make_layer(self, block, out_channels, num_blocks, stride):
-        strides = [stride] + [1] * (num_blocks - 1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_channels, out_channels, stride))
-            self.in_channels = out_channels * block.expansion
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = F.relu(out)
-        #out = self.maxpool(out)
-
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out = self.fc(out)
-        return out
+from torchvision import models
 
 class SiameseNetwork(nn.Module):
     def __init__(self) -> None:
         super().__init__()
 
-        self.layers = nn.Sequential(
-            
-        )
+        # Pretrained model
+        resnet50= models.resnet50(weights="ResNet50_Weights.DEFAULT")
+        a = 2
 
     def base_network(self):
         return
 
     def forward(self):
         return
+    
+class TripletLoss(nn.Module):
+    def __init__(self, margin = 1) -> None:
+        super(TripletLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, anchor: torch.Tensor, positive: torch.Tensor, negative: torch.Tensor) -> torch.Tensor:
+        """L(triplet) = max(0, d(a,p) - d(a,n) + m)""" 
+        loss = torch.Tensor(0)
+        return loss
+        
+
+        
+
+        
