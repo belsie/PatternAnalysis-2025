9143b0d4723db30f241f2cf8f99221a8ebfcf56d
belle 3 <annabelle.mouat@gmail.com>
2025-10-25 21:32:10 +1000

    Implemented basic siamese network for use in training

--- recognition/siamese_network_47452109/modules.py
+++ recognition/siamese_network_47452109/modules.py
@@ -8,19 +8,34 @@ import torch.nn.functional as F
 import torch
 from torchvision import models
 
-class SiameseNetwork(nn.Module):
-    def __init__(self) -> None:
+class Network(nn.Module):
+    def __init__(self, embed_dim = 128, norm= True) -> None:
         super().__init__()
 
         # Pretrained model
         resnet50= models.resnet50(weights="ResNet50_Weights.DEFAULT")
-        a = 2
+        
+        # remove classifier head
+        modules = list(resnet50.children())[:-1]
+        self.encoder = nn.Sequential(*modules)
+        self.proj = nn.Linear(2048, embed_dim)
+        self.norm = norm
 
-    def base_network(self):
-        return
+    def base_network(self, x: torch.Tensor) -> torch.Tensor:
+        enc = self.encoder(x)
+        enc = torch.flatten(enc, 1)
+        embed = self.proj(enc)
 
-    def forward(self):
-        return
+        if self.norm:
+            embed = F.normalize(embed, p=2, dim=1)
+        return embed
+
+    def forward(self, x1: torch.Tensor, x2: torch.Tensor|None):
+        e1 = self.base_network(x1)
+        if x2 is None: 
+            return e1
+        e2 = self.base_network(x2)
+        return e1, e2
     
 class TripletLoss(nn.Module):
     def __init__(self, margin = 1) -> None:
@@ -28,11 +43,15 @@ class TripletLoss(nn.Module):
         self.margin = margin
 
     def forward(self, anchor: torch.Tensor, positive: torch.Tensor, negative: torch.Tensor) -> torch.Tensor:
-        """L(triplet) = max(0, d(a,p) - d(a,n) + m)""" 
-        loss = torch.Tensor(0)
-        return loss
+        """L(triplet) = max(0, distance(anchor,positive) - distance(anchor,negative) + margin)""" 
+        def _distance(a, b):
+            difference = a - b
+            return (difference * difference).sum(dim=1).sqrt()   
         
+        pos_dist = _distance(anchor, positive)
+        neg_dist = _distance(anchor, negative)
 
-        
+        loss = F.relu(pos_dist - neg_dist + self.margin)
 
+        return loss.mean()
         
